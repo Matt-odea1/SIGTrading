c8fb9b1622437e14c3a0a9c417412b217ae4fad7
Matt O'Dea <z5413887@ad.unsw.edu.au>
2024-06-20 14:25:40 +1000

    environment setup

---
+++ GlobleTrading.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+import pandas as pd
+
+##### TODO #########################################
+### RENAME THIS FILE TO YOUR TEAM NAME #############
+### IMPLEMENT 'getMyPosition' FUNCTION #############
+### TO RUN, RUN 'eval.py' ##########################
+
+nInst = 50
+currentPos = np.zeros(nInst)
+
+def loadPrices(fn):
+    global nt, nInst
+    df = pd.read_csv(fn, sep='\s+', header=None, index_col=None)
+    (nt, nInst) = df.shape
+    return (df.values).T
+
+
+
+def getMyPosition(prcSoFar):
+    global currentPos
+    (nins, nt) = prcSoFar.shape
+    if (nt < 2):
+        return np.zeros(nins)
+    lastRet = np.log(prcSoFar[:, -1] / prcSoFar[:, -2])
+    lNorm = np.sqrt(lastRet.dot(lastRet))
+    lastRet /= lNorm
+    rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:, -1]])
+    currentPos = np.array([int(x) for x in currentPos+rpos])
+    return currentPos
+
+print("hello world!")
+
+pricesFile = "./prices.txt"
+prcAll = loadPrices(pricesFile)
+print("Loaded %d instruments for %d days" % (nInst, nt))
